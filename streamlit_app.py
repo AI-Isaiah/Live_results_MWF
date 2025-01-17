f5f59ed756342d988c8c218f3e531482f4a75713
AI-Isaiah <117355363+AI-Isaiah@users.noreply.github.com>
2025-01-17 16:41:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("OKX Streamlit Demo")
+
+api_key = st.secrets["OKX"]["API_KEY"]
+secret_key = st.secrets["OKX"]["SECRET_KEY"]
+passphrase = st.secrets["OKX"]["PASSPHRASE"]
+
+st.write("API Key:", api_key)
+st.write("Secret Key:", secret_key)
+st.write("Passphrase:", passphrase)
